d44b1fdfa40da8e8634820be5a2509f95b1c61a3
pasqwal <courty.pascal@gmail.com>
2021-11-30 15:31:17 +0100

    add simple game

---
+++ game.py
@@ -0,0 +1,24 @@
+# pylint: disable=missing-docstring
+# pylint: disable=too-few-public-methods
+
+import string
+import random
+
+
+class Game:
+    def __init__(self):
+        self.grid = []
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
+
+
+    def is_valid(self, word):
+        if not word:
+            return False
+        letters = self.grid.copy()
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+        return True
